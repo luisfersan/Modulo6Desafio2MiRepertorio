4bc66680ea834a1ad1988bb2618bc1aa8a0c14c0
DanielHernandezToledo <daniel.aht1993@gmail.com>
2024-10-05 20:42:58 -0300

    creacion de metodos put y delete

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
-
 const fs = require('fs')
-
 const path = require('path')
-
 const app = express()
+
+app.use(express.json())
 
 app.listen(3000, () => {
   console.log('Servidor funcionando en el puerto 3000')
@@ -16,7 +15,42 @@ app.get('/', (req, res) => {
   res.sendFile(index)
 })
 
-app.get('/canciones', (req, res) => {
-  const canciones = JSON.parse(fs.readFileSync('./repertorio.json'))
+app.get('/canciones', async(req, res) => {
+  const canciones = await JSON.parse(fs.readFileSync('./repertorio.json'))
   res.send(canciones)
 })
+
+// app.post('/canciones', async(req, res) => {
+//   const cancion = req.body
+//   const canciones = await JSON.parse(fs.readFileSync('./repertorio.json'))
+//   canciones.push(cancion)
+//   fs.writeFileSync('./repertorio.json', JSON.stringify(canciones))
+//   res.send(cancion)
+// })
+
+app.put('/canciones/:id', async(req, res) => {
+  const id = parseInt(req.params.id)
+  console.log(id, '<---- id')
+  let canciones = await JSON.parse(fs.readFileSync('./repertorio.json'))
+  const cancion = canciones.find((cancion) => {cancion.id === id})
+
+  canciones = canciones.map((cancion) => {
+    if (cancion.id === id) {
+      return {
+        ...cancion,
+        ...req.body
+      }
+    }
+    return cancion
+  })
+
+  fs.writeFileSync('./repertorio.json', JSON.stringify(canciones))
+  res.send(cancion)
+})
+
+app.delete('/canciones/:id', async(req, res) => {
+  const id = parseInt(req.params.id)
+  let canciones = await JSON.parse(fs.readFileSync('./repertorio.json'))
+  canciones = canciones.filter((cancion) => cancion.id !== id)
+  fs.writeFileSync('./repertorio.json', JSON.stringify(canciones))
+  res.send(canciones)})
